600fe9996d7471b82330bf831d363e01804c9a32
AlexDark <alexdarkovich@mail.ru>
2023-03-14 18:16:33 +0300

    Composite and Chain of responsibility patterns implemented3

--- task2-observer-pattern/src/main/java/com/mjc/stage2/impl/RectangleObserver.java
+++ task2-observer-pattern/src/main/java/com/mjc/stage2/impl/RectangleObserver.java
@@ -2,20 +2,36 @@ package com.mjc.stage2.impl;
 
 import com.mjc.stage2.Observer;
 import com.mjc.stage2.entity.Rectangle;
+import com.mjc.stage2.entity.RectangleValues;
 import com.mjc.stage2.event.RectangleEvent;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 public class RectangleObserver implements Observer {
+    private Map<Integer, RectangleValues> rectangleWarehouse;
+
+    public RectangleObserver() {
+        rectangleWarehouse = new HashMap<>();
+    }
+
     @Override
     public void handleEvent(RectangleEvent event) {
         Rectangle rectangle = event.getSource();
-        int id = rectangle.getId();
+        int rectangleId = rectangle.getId();
         double sideA = rectangle.getSideA();
         double sideB = rectangle.getSideB();
-        double square = sideA * sideB;
-        double perimeter = (sideA + sideB) * 2;
-        System.out.println("square = " + square);
-        System.out.println("perimeter = " + perimeter);
 
+        double square = sideA * sideB;
+        double perimeter = 2 * (sideA + sideB);
+
+        RectangleValues values = new RectangleValues(square, perimeter);
+        rectangleWarehouse.put(rectangleId, values);
+    }
+
+    public Map<Integer, RectangleValues> getRectangleWarehouse() {
+        return Collections.unmodifiableMap(rectangleWarehouse);
     }
     // Write your code here!
 }
